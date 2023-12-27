e48c602263ea658bfdaa7fc85f7c0b18f2ccf5e5
kiichi630 <nakaichan0630@gmail.com>
2023-12-27 20:38:36 +0900

    ActiveHashの導入、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :area
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
+
+  validates :product, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :area_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
 end
