29272070b4d107b298c6379c78b63598ac6b30ac
kiichi630 <nakaichan0630@gmail.com>
2024-01-09 02:30:32 +0900

    itemモデルのテスト実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :product, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: true, format: { with: /\A[0-9]+\z/ }
+  validates_inclusion_of :price, in:300..9999999
   validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :area_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank" }
